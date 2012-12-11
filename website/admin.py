1c56b5b88950e6d20cb36d6c6c9da9604170a5cc
paslo22 <paslo@paslo.(none)>
2012-12-11 19:25:46 -0300

    Terminada la sincronizacion con el repo original

--- website/admin.py
+++ website/admin.py
@@ -5,9 +5,3 @@ admin.site.register(Producto)
 admin.site.register(Seccion)
 admin.site.register(Campania)
 admin.site.register(Fondo)
-
-
-<<<<<<< HEAD
-=======
-
->>>>>>> c568ce6b3c2c79adb4ef7e114d5c05c6fa3acf29
--- website/views.py
+++ website/views.py
@@ -1,31 +1,4 @@
 from website.models import *
-<<<<<<< HEAD
-from django.shortcuts import render_to_response, get_object_or_404
-from django.http import HttpResponse
-from django.template import RequestContext
-from django.core import serializers
-
-def secciones(request):
-	secciones= serializers.serialize('json', Seccion.objects.all().reverse()) 
-	return HttpResponse(simplejson.dumps(secciones), mimetype='application/json')
-
-
-def productos(request, id_seccion):
-	secciones= serializers.serialize('json', Producto.objects.filter(secciones= id_seccion))
-	return HttpResponse(simplejson.dumps(productos), mimetype='application/json')
-
-
-
-
-def campania(request):
-	campania= serializers.serialize('json', Campania.objects.all().reverse()) 
-	return HttpResponse(simplejson.dumps(campania), mimetype='application/json')
-
-
-def fondos(request):
-	fondos= serializers.serialize('json', Fondo.objects.all()) 
-	return HttpResponse(simplejson.dumps(fondos), mimetype='application/json')
-=======
 from django.http import HttpResponse
 from django.core import serializers
 from django.utils import simplejson
@@ -53,4 +26,4 @@ def fondos(request):
 
     fondos = serializers.serialize('json', Fondo.objects.all()) 
     return HttpResponse(simplejson.dumps(fondos), mimetype='application/json')
->>>>>>> c568ce6b3c2c79adb4ef7e114d5c05c6fa3acf29
+
