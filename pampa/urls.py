405b53a430b4eb112292d72c5d03cded144078c5
Leonardo Ferreyra <leferreyra@gmail.com>
2012-12-11 21:28:42 -0300

    Agrega configuraciones para servir archivos media, con el servidor de desarrollo

--- pampa/urls.py
+++ pampa/urls.py
@@ -17,3 +17,17 @@ urlpatterns = patterns('',
     url(r'^campaign/', "website.views.campania"),
     url(r'^background/', "website.views.fondos"),
 )
+
+
+# Configuraciones adicionales para servir archivos media,
+# cuando trabajamos con el servidor de desarrollo
+if settings.DEBUG:
+    from django.views.static import serve
+    _media_url = settings.MEDIA_URL
+    if _media_url.startswith('/'):
+        _media_url = _media_url[1:]
+        urlpatterns += patterns('',
+                                (r'^%s(?P<path>.*)$' % _media_url,
+                                serve,
+                                {'document_root': settings.MEDIA_ROOT}))
+    del(_media_url, serve)
