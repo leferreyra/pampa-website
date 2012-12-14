474cc8ef693f7a83feab63ad4d53699c89fe6365
Paulo Muchutti <paslo22@gmail.com>
2012-12-14 01:31:57 -0300

    Comenzando las funciones JS para carga de Hashes

--- static/js/hashChanger.js
+++ static/js/hashChanger.js
@@ -1,10 +1,22 @@
 $(document).ready(function () {
 	var hash = document.location.hash;
+	$('.menu').addClass('show');
 	if (hash) {
 		loadHash(hash)
 	};
 })
 
 function loadHash (hash) {
-	alert(hash)
+	go_to[hash]();
+}
+
+var go_to = {
+	'#!/collection' : function() {
+		//showLoading();
+		$.getJSON('collection/', function(data) {
+			json = $.parseJSON(data);
+			alert(typeof(json['nombre']))
+		})
+		//hideLoading();
+	}
 }
